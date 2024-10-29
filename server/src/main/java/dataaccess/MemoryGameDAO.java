0a32ceacdb4144c7b4f6d8efc1e6f898d2457d7a
Doyoung Yoon <qklzd13@gmail.com>
2024-10-29 08:52:12 -0600

    persistence test passed

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -2,6 +2,7 @@ package dataaccess;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 import model.result.GameResult;
 
@@ -10,6 +11,22 @@ import java.util.random.RandomGenerator;
 
 public class MemoryGameDAO implements GameDAO{
     private final Map<Integer, GameData> gameStore = new HashMap<>();
+
+    public MemoryGameDAO() throws DataAccessException {
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  games (
+              `gameID` int NOT NULL AUTO_INCREMENT,
+              `whiteUsername` TEXT DEFAULT NULL,
+              `blackUsername` TEXT DEFAULT NULL,
+              `gameName` TEXT DEFAULT NULL CHECK (`gameName` <> ''),
+              `game` TEXT DEFAULT NULL,
+              PRIMARY KEY (`gameID`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+        };
+        DatabaseManager.configureDatabase(createStatements);
+    }
     @Override
     public GameData getGame(int gameId) throws DataAccessException {
         return gameStore.get(gameId);
@@ -39,6 +56,9 @@ public class MemoryGameDAO implements GameDAO{
         int gameID = RandomGenerator.getDefault().nextInt(100);
         GameData game = new GameData(gameID, null, null, gameName, newGame);
         gameStore.put(gameID, game);
+        var statement = "INSERT INTO games (whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?)";
+        String gameJson = new Gson().toJson(game.game());
+        DatabaseManager.executeUpdate(statement, game.whiteUserName(), game.blackUserName(), game.gameName(), gameJson);
         return new GameData(gameID, null, null, gameName, newGame);
     }
 
@@ -50,6 +70,7 @@ public class MemoryGameDAO implements GameDAO{
 
     @Override
     public void clear() throws DataAccessException {
-        gameStore.clear();
+        String[] statement = new String[]{"TRUNCATE games"};
+        DatabaseManager.configureDatabase(statement);
     }
 }
