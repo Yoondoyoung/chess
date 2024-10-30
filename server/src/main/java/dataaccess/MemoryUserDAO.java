e13b00911f935eab7df97e97d0534b8e8cdfb2f8
Doyoung Yoon <qklzd13@gmail.com>
2024-10-29 21:36:58 -0600

    DB for user making

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,23 +1,61 @@
 package dataaccess;
 
 import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
 import java.util.Map;
 import java.util.HashMap;
 public class MemoryUserDAO implements UserDAO{
     private final Map<String, UserData> userStore = new HashMap<>();
+
+    public MemoryUserDAO() throws DataAccessException {
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(255) NOT NULL,
+              `password` TEXT NOT NULL,
+              `email` TEXT NOT NULL,
+              PRIMARY KEY (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+        };
+        DatabaseManager.configureDatabase(createStatements);
+    }
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return userStore.get(username);
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users WHERE username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        var password = rs.getString("password");
+                        var email = rs.getString("email");
+                        return new UserData(username, password, email);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-        userStore.put(user.username(), user);
+        try {
+            var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+
+            String passwordHash = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+            var id = DatabaseManager.executeUpdate(statement, user.username(), passwordHash, user.email());
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
     }
 
     public void clear() throws DataAccessException {
-        userStore.clear();
+        var statement = "TRUNCATE users";
+        DatabaseManager.executeUpdate(statement);
     }
 
 }
